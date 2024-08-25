925ca3593107aaac552cbdf1d5d6679fa598d846
rupokghosh <rupokghosh291@gmail.com>
2024-08-24 20:00:31 -0700

    finish projects

---
+++ src/components/projectCard.tsx
@@ -0,0 +1,36 @@
+import { FaGithub, FaArrowRight } from "react-icons/fa";
+import Link from "next/link";
+
+interface ProjectCardProps {
+  title: string;
+  description: string;
+  link: string;
+  github?: string; // Optional GitHub link
+}
+
+function ProjectCard({ title, description, link, github }: ProjectCardProps) {
+  return (
+    <div className="relative bg-gray-400 mx-8 sm:w-80 md:w-96 h-64 rounded-lg text-black border border-gray-800 shadow-lg transition duration-300 ease-in hover:bg-black hover:text-white hover:shadow-xl">
+      <div className="absolute top-2 right-2 p-2">
+        {github ? (
+          <Link href={github} passHref className="cursor-pointer">
+            <FaGithub size={24} className="hover:text-white" />
+          </Link>
+        ) : (
+          <FaGithub size={24} className="text-gray-800" />
+        )}
+      </div>
+      <div className="p-4 h-full flex flex-col justify-between">
+        <h2 className="text-lg sm:text-xl font-bold">{title}</h2>
+        <p className="text-sm mt-2">{description}</p>
+        <Link href={link} target="_blank" rel="noopener noreferrer">
+          <div className="absolute bottom-4 flex items-center text-md sm:text-lg bg-transparent cursor-pointer hover:underline">
+            Explore <FaArrowRight className="ml-2" />
+          </div>
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+export default ProjectCard;
