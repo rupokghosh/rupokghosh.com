eaa2ff97c858c447608f7e9448beb9d9560e4a71
rupokghosh <rupokghosh291@gmail.com>
2024-08-25 12:43:28 -0700

    add github to contacts list

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,6 +1,6 @@
 import Nav from "@/components/navigation";
 import Link from "next/link";
-import { FaTwitter, FaLinkedin, FaEnvelope } from "react-icons/fa";
+import { FaTwitter, FaLinkedin, FaEnvelope, FaGithub } from "react-icons/fa";
 
 const containerClasses = "flex flex-col md:flex-row justify-center items-center gap-8 md:gap-12 md:mt-40 mt-12 mb-8 px-4";
 const cardClasses = "flex flex-col gap-4 items-center justify-center w-full md:w-64 md:h-64 bg-black text-white rounded-lg md:border border-gray-600 shadow-lg cursor-pointer p-4 transition duration-300 hover:bg-gray-800";
@@ -32,6 +32,13 @@ const ContactPage = () => {
             <FaEnvelope size={iconSize} />
             <h2 className={titleClasses}>Email</h2>
             <p className={textClasses}>rupokghosh291@gmail.com</p>
+          </div>
+        </Link>
+        <Link href="https://github.com/rupokghosh" passHref>
+          <div className={cardClasses}>
+            <FaGithub size={iconSize} />
+            <h2 className={titleClasses}>GitHub</h2>
+            <p className={textClasses}>rupokghosh</p>
           </div>
         </Link>
       </div>
